dab2d56b0d48f90b9a776765eeb8de67aba403b1
xavier.didelot@gmail.com <xavier.didelot@gmail.com@3ae39ca0-168b-8fd5-bc72-1a53c72e3606>
2014-02-12 13:05:17 +0000

    Replaced SIR model with renewal model

--- trunk/main.m
+++ trunk/main.m
@@ -3,18 +3,13 @@ close all hidden force
 rand('state',0);
 randn('state',0);
 
-%Biological parameters, calibrated for Staphylococcus aureus,  based on 
-%Young et al. (2012) PNAS 109:4550-4555
-%Golubchik et al. (2013) PLoS One 8:e61319
 neg=100/365;%Within-host effective population size (Ne) times  generation duration (g)
-popsize=1000;%Size of population
-beta=0.001;%Infectiveness
-nu=1;%Rate of recovery
+R=1;%Basic reproduction number
 
 %Create a transmission tree with ten individuals
 n=1;
 while n~=10
-    ttree=makeTTree(popsize,beta,nu);
+    ttree=makeTTree(R);
     n=size(ttree,1);
 end
 
@@ -31,24 +26,21 @@ timeLastRem=max(truth(:,1));
 
 %MCMC loop
 fulltree=makeFullTreeFromPTree(ptreeFromFullTree(truth));%Starting point
-mcmc=10000;
+mcmc=1000;
 record(mcmc).tree=0;
-pTTree=probTTree(ttreeFromFullTree(fulltree),popsize,beta,nu);
+pTTree=probTTree(ttreeFromFullTree(fulltree),R);
 pPTree=probPTreeGivenTTree(fulltree,neg);
 for i=1:mcmc
     if mod(i,100)==0,i,end
-    %if sanitycheck(fulltree)==0,break;end
     %Record things
     record(i).tree=absolute(fulltree,timeLastRem);
     record(i).pTTree=pTTree;
     record(i).pPTree=pPTree;
     record(i).neg=neg;
     record(i).source=fulltree(end-1,4);
-    record(i).nu=nu;
-    record(i).beta=beta;
     %Metropolis update for transmission tree
     fulltree2=proposal(fulltree);
-    pTTree2=probTTree(ttreeFromFullTree(fulltree2),popsize,beta,nu);
+    pTTree2=probTTree(ttreeFromFullTree(fulltree2),R);
     pPTree2=probPTreeGivenTTree(fulltree2,neg);
     if log(rand)<pTTree2+pPTree2-pTTree-pPTree,fulltree=fulltree2;pTTree=pTTree2;pPTree=pPTree2;end
     %Metropolis update for Ne*g, assuming Exp(1) prior
@@ -56,11 +48,6 @@ for i=1:mcmc
     if neg2<0,neg2=-neg2;end
     pPTree2=probPTreeGivenTTree(fulltree,neg2);
     if log(rand)<pPTree2-pPTree-neg2+neg,neg=neg2;pPTree=pPTree2;end
-    %Gibbs updates for beta and nu based on O'Neill and Roberts JRSSA 16:121-129 (1999) and assuming Exp(1) priors
-    ttree=ttreeFromFullTree(fulltree);
-    intXtYtdt=intXtYtdtGivenTTree(ttree,popsize);
-    beta=gamrnd(1+n-1,1/(1+intXtYtdt));
-    nu=gamrnd(1+n,1/(1+n*mean(ttree(:,2)-ttree(:,1))));
 end
 
 ft=absolute(consensus(record),timeLastRem);
@@ -90,12 +77,6 @@ title('Log-posterior');
 subplot(2,3,2);
 plot([record(:).neg]);
 title('Parameter Ne*g');
-subplot(2,3,3);
-plot([record(:).beta]);
-title('Parameter beta');
-subplot(2,3,4);
-plot([record(:).nu]);
-title('Parameter nu');
 subplot(2,3,5);
 plot([record(:).source]);
 title('Source case');
--- trunk/probTTree.m
+++ trunk/probTTree.m
@@ -1,27 +1,12 @@
-function prob=probTTree(ttree,popsize,beta,nu)
-%Calculate the log-probability of a transmission tree given popsize, beta and nu
-
-for i=1:size(ttree,1)
-    infector=ttree(i,3);
-    if infector==0,continue;end
-    if ttree(i,1)>ttree(infector,2)||ttree(i,1)<ttree(infector,1),prob=-inf;return;end
-end
-
-[times,ind]=sort([ttree(:,1);ttree(:,2)]);%Event times
-types=ind>size(ttree,1);%0 for infection, 1 for recovery
-sir=[popsize-1 1 0];
+function prob=probTTree(ttree,R)
+%Calculate the log-probability of a transmission tree given R
+%Assume w is Gamma(2,1)
 prob=0;
-for i=2:length(times)
-    rate=nu*sir(2)+beta*sir(1)*sir(2);
-    prob=prob+log(rate)-rate*(times(i)-times(i-1));%;log(exppdf(times(i)-times(i-1),1/rate));
-    prob=prob+log(1/sir(2));
-    if types(i)==1
-        %Recovery
-        prob=prob+log(nu*sir(2)/rate);
-        sir(3)=sir(3)+1;sir(2)=sir(2)-1;
-    else
-        %Infection
-        prob=prob+log(1-nu*sir(2)/rate);
-        sir(1)=sir(1)-1;sir(2)=sir(2)+1;
+for i=1:size(ttree,1)
+    prob=prob+log(gampdf(ttree(i,2)-ttree(i,1),2,1));
+    offspring=find(ttree(:,3)==i);
+    prob=prob+log(poisspdf(length(offspring),R));
+    for j=offspring'
+        prob=prob+log(gampdf(ttree(j,1)-ttree(i,1),2,1));
     end
 end
