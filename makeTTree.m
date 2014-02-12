dc15f16c10f0a1409ad844ce4cdef520d510b642
xavier.didelot <xavier.didelot@gmail.com>
2014-02-12 13:05:17 +0000

    Replaced SIR model with renewal model

--- makeTTree.m
+++ makeTTree.m
@@ -1,35 +1,27 @@
-function [ttree,prob]=makeTTree(popsize,beta,nu)
+function [ttree,prob]=makeTTree(R)
 %Creates a transmission tree, and returns the result as a N*3 matrix in the
 %following format:
 %One row per infected host
 %First column is time of infection
-%Second column is time of recovery
+%Second column is time of sampling
 %Third column is infector
-%nu is the rate of recovery
-%beta is the rate of infection
-%popsize is the total size of population
+%R is the basic reproduction number
+%Assume w is Gamma(2,1)
 ttree=[0 0 0];
-sir=[popsize-1 1 0];
-curt=0;
 prob=0;
-while 1
-    rate=nu*sir(2)+beta*sir(1)*sir(2);
-    if rate==0,break;end
-    t=exprnd(1/rate);
-    prob=prob+log(exppdf(t,1/rate));
-    curt=curt+t;
-    w=find(ttree(:,2)==0);%All infected
-    prob=prob+log(1/length(w));
-    w=w(1+floor(length(w)*rand));%Pick one at random
-    if rand<nu*sir(2)/rate
-        %Recovery
-        prob=prob+log(nu*sir(2)/rate);
-        ttree(w,2)=curt;
-        sir(3)=sir(3)+1;sir(2)=sir(2)-1;
-    else
-        %Infection
-        prob=prob+log(1-nu*sir(2)/rate);
-        ttree=[ttree;curt 0 w];
-        sir(1)=sir(1)-1;sir(2)=sir(2)+1;
+todo=1;
+while ~isempty(todo)
+    draw=gamrnd(2,1);
+    ttree(todo(1),2)=ttree(todo(1),1)+draw;
+    prob=prob+log(gampdf(draw,2,1));
+    offspring=poissrnd(R);
+    prob=prob+log(poisspdf(offspring,R));
+    for i=1:offspring
+        draw=gamrnd(2,1);
+        prob=prob+log(gampdf(draw,2,1));
+        ttree=[ttree;ttree(todo(1),1)+draw 0 todo(1)];
+        if size(ttree,1)>100,ttree=[];return;end
+        todo=[todo size(ttree,1)];
     end
+    todo=todo(2:end);
 end
