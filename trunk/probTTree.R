d62abf100f542e8b8a02c5eed14eddaf27d9f7e6
xavier.didelot@gmail.com <xavier.didelot@gmail.com@3ae39ca0-168b-8fd5-bc72-1a53c72e3606>
2014-02-13 14:40:32 +0000

    Converted probTTree to R

---
+++ trunk/probTTree.R
@@ -0,0 +1,14 @@
+probTTree = function(ttree,R)  {
+#Calculate the log-probability of a transmission tree given R 
+#Assume w is Gamma(2,1) 
+prob <- 0 
+for (i in (1:nrow(ttree))) { 
+    prob <- prob + log(dgamma((ttree[i,2]-ttree[i,1]), shape = 2, scale = 1)) 
+    offspring <- which( cbind(ttree[ ,3]) == i ) 
+    prob <- prob + log(dpois(length(offspring),R)) 
+    for (j in (offspring)) {
+        prob <- prob + log(dgamma((ttree[j,1]-ttree[i,1]), shape = 2, scale = 1)) 
+    } 
+} 
+return(prob)
+} 
