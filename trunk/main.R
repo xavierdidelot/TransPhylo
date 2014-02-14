b18d52a10dd7f78d195b1d3eb706effd2774d304
xavier.didelot@gmail.com <xavier.didelot@gmail.com@3ae39ca0-168b-8fd5-bc72-1a53c72e3606>
2014-02-14 15:58:01 +0000

    First functioning version in R !

--- trunk/main.R
+++ trunk/main.R
@@ -8,6 +8,7 @@ source('ttreeFromFullTree.R')
 source('probPTreeGivenTTree.R')
 source('pTreeFromFullTree.R')
 source('makeFullTreeFromPTree.R')
+source('proposal.R')
 neg<-100/365#Within-host effective population size (Ne) times  generation duration (g)
 R<-1#Basic reproduction number
 
@@ -28,5 +29,43 @@ for (i in (1:n)) {
 
 #Glue these trees together
 truth<-glueTrees(ttree,wtree)
-truth[,1]<-truth[,1]+2005#Epidemic started in 2005
-makeFullTreeFromPTree(ptreeFromFullTree(truth))
+#truth[,1]<-truth[,1]+2005#Epidemic started in 2005
+
+#MCMC loop 
+#fulltree <- makeFullTreeFromPTree(ptreeFromFullTree(truth));#Starting point 
+fulltree<-truth
+mcmc <- 1000 
+record <- vector('list',mcmc)
+pTTree <- probTTree(ttreeFromFullTree(fulltree),R) 
+pPTree <- probPTreeGivenTTree(fulltree,neg) 
+for (i in 1:mcmc) { 
+  if (i%%100 == 0)  { 
+    print(i) 
+  } 
+  #Record things 
+  record[[i]]$tree <- fulltree
+  record[[i]]$pTTree <- pTTree 
+  record[[i]]$pPTree <- pPTree 
+  record[[i]]$neg <- neg 
+  record[[i]]$source <- fulltree[nrow(fulltree)-1,4] 
+  #Metropolis update for transmission tree 
+  fulltree2 <- proposal(fulltree) 
+  pTTree2 <- probTTree(ttreeFromFullTree(fulltree2),R) 
+  pPTree2 <- probPTreeGivenTTree(fulltree2,neg) 
+  if (log(runif(1)) < pTTree2 + pPTree2-pTTree-pPTree)  { 
+    fulltree <- fulltree2 
+    pTTree <- pTTree2 
+    pPTree <- pPTree2 
+  } 
+  #Metropolis update for Ne*g,assuming Exp(1) prior 
+  neg2 <- neg + (runif(1)-0.5)*record[[1]]$neg 
+  if (neg2 < 0)  { 
+    neg2 <- -neg2 
+  } 
+  pPTree2 <- probPTreeGivenTTree(fulltree,neg2) 
+  if (log(runif(1)) < pPTree2-pPTree-neg2 + neg)  { 
+    neg <- neg2 
+    pPTree <- pPTree2 
+  } 
+} 
+
