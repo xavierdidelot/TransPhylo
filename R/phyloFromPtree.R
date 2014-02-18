d17687a5a3463956d88d0026abcc7c99eda717ce
xavier.didelot <xavier.didelot@gmail.com>
2014-02-18 15:58:43 +0000

    Added conversion to phylo object from ape package

---
+++ R/phyloFromPtree.R
@@ -0,0 +1,23 @@
+#' Converts a phylogenetic tree into an ape phylo object
+#' @param ptree phylogenetic tree
+#' @return phylo object
+phyloFromPtree <- function(ptree) {
+  n<-ceiling(nrow(ptree)/2)
+  tr<-list()
+  tr$Nnode<-n
+  tr$tip.label<-as.character(1:n)
+  tr$edge<-matrix(0,n*2-2,2)
+  tr$edge.length<-rep(0,n*2-2)
+  iedge<-1
+  tra<-c(1:n,(2*n-1):(n+1))
+  for (i in nrow(ptree):(n+1)) {
+    tr$edge[iedge,]<-c(tra[i],tra[ptree[i,2]])
+    tr$edge.length[iedge]<-ptree[ptree[i,2],1]-ptree[i,1]
+    iedge<-iedge+1
+    tr$edge[iedge,]<-c(tra[i],tra[ptree[i,3]])
+    tr$edge.length[iedge]<-ptree[ptree[i,3],1]-ptree[i,1]
+    iedge<-iedge+1
+  }
+  class(tr)<-'phylo'
+  return(tr)
+}
