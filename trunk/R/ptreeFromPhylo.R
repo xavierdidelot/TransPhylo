c6618feb76e7d75785a94c97ba242e552656ad33
xavier.didelot@gmail.com <xavier.didelot@gmail.com@3ae39ca0-168b-8fd5-bc72-1a53c72e3606>
2014-02-19 14:18:59 +0000

    Convert phylo to ptree

---
+++ trunk/R/ptreeFromPhylo.R
@@ -0,0 +1,8 @@
+#' Converts an ape phylo object into a phylogenetic tree
+#' @param phy phylo object
+#' @return phylogenetic tree
+#' @return phylo object
+ptreeFromPhylo <- function(phy) {
+  
+  return(ptree)
+}
