0d532ace2113c7ceb813b6b5cc21cd444d429477
xavier.didelot@gmail.com <xavier.didelot@gmail.com@3ae39ca0-168b-8fd5-bc72-1a53c72e3606>
2014-07-01 15:30:39 +0000

    Improved calculation of p(P|T)

--- trunk/R/probPTreeGivenTTree.R
+++ trunk/R/probPTreeGivenTTree.R
@@ -5,54 +5,46 @@
 probPTreeGivenTTree = function(fulltree,neg)  {
   n <- max(fulltree[,4])
   prob <- 0 
-  fathers <- rep(NA, nrow(fulltree)) 
-  fathers[fulltree[ ,2] + 1] <- 1:nrow(fulltree) 
-  fathers[fulltree[ ,3] + 1] <- 1:nrow(fulltree) 
-  fathers <- fathers[-1] 
   for (i in (1:n)) { 
-    subtree <- .extractSubtree(fulltree,i,fathers) 
+    subtree <- .extractSubtree(fulltree,i) 
     prob <- prob + .probSubtree(subtree,neg) 
   } 
   return(prob)
 } 
 
-.extractSubtree = function(fulltree,which,fathers)  {
+.extractSubtree = function(fulltree,w)  {
   #Take all nodes in host 
-  host <- cbind(fulltree[ ,4]) 
-  ind <- 1:nrow(fulltree) 
-  ind <- ind[which(host == which)] 
+  ind <- which(fulltree[,4] == w)
   #Add father of oldest node 
-  j <- which.min(fulltree[ind])
-  ind <- c(ind,fathers[ind[j]]) 
+  ind <- c(ind,which(fulltree[,2]==ind[length(ind)]|fulltree[,3]==ind[length(ind)]))
   #Create subtree 
   subtree <- matrix(0, length(ind), 2) 
-  invind <- rep(0, nrow(fulltree)) 
-  invind[ind] <- 1:length(ind) 
   for (i in 1:length(ind)) { 
-    subtree[i,1] <- fulltree[ind[i],1] 
-    if (i < length(ind))  { 
-      subtree[i,2] <- invind[fathers[ind[i]]] 
-    } 
+    subtree[i,1] <- fulltree[ind[i],1]
+    subtree[which(ind==fulltree[ind[i],2]),2]=i
+    subtree[which(ind==fulltree[ind[i],3]),2]=i
   } 
   return(subtree)
 } 
 
 .probSubtree = function(tab,rate)  {
-  #tab(:,1)=age at bottom;tab(:,2)=father;rate=coalescence rate 
+  #tab(:,1)=times at bottom;tab(:,2)=father;rate=coalescence rate 
   #Return the log-prior probability of a subtree 
-  #This is an extension to Eq1 of Drummond et al(2002)Genetics 
-  #161:1307-1320 that accounts for condition TMRCA<INCUBATION_PERIOD 
+  #This is an extension to Eq1 of Drummond et al(2002) Genetics 161:1307-1320 that accounts for condition TMRCA<INCUBATION_PERIOD 
   p <- 0 
   tab[ ,1] <- max(tab[ ,1])-tab[ ,1];#convert times to ages
   isiso <- rep(1, nrow(tab)) 
   isiso[tab[1:(nrow(tab)-1),2]] <- 0 
-  iso <- which(isiso==1) 
-  ex <- rep(0, nrow(tab));#Keep track of which nodes are active 
-  MySort <- sort(tab[ ,1],index.return = TRUE); s <- MySort$x; ind <- MySort$ix 
-  cur <- iso[1];while (tab[cur,2] > 0) {ex[cur] <- 1;cur <- tab[cur,2]};ex[length(ex)] <- 1; 
-  for (l in (.seqML(2,length(iso),1))) {#For all others leaves in increasing order of age 
-    isanc <- rep(0, nrow(tab));#Ancestors of the current node 
-    anc <- iso[l] 
+  ex <- rep(0, nrow(tab))#Keep track of which nodes are active 
+  #MySort <- sort(tab[ ,1],index.return = TRUE); ind <- MySort$ix 
+  ind=c(1+which(tab[2:nrow(tab),1]<tab[1,1]),1,1+which(tab[2:nrow(tab),1]>tab[1,1]))#The tab matrix should be already ordered, except maybe the first leaf corresponding to a sample
+  iso=c(which(tab[,1]<tab[1,1]&isiso),1,which(tab[,1]>tab[1,1]&isiso))#List of leaves in increasing order of age
+  cur <- iso[1]#Start with youngest leaf
+  while (tab[cur,2] > 0) {ex[cur] <- 1;cur <- tab[cur,2]}#Activate path to root
+  ex[length(ex)] <- 1#Activate root
+  for (l in iso[-1]) {#For all leaves in increasing order of age
+    isanc <- rep(0, nrow(tab))#Ancestors of the current node 
+    anc <- l
     while (tab[anc,2] > 0)  { 
       isanc[anc] <- 1 
       anc <- tab[anc,2] 
@@ -63,35 +55,34 @@ probPTreeGivenTTree = function(fulltree,neg)  {
     found <- FALSE 
     curage <- 0 
     k <- 0 
-    for (i in (1:(length(s)))) { 
-      if (ind[i] == iso[l])  { 
+    for (i in ind) {
+      if (i == l)  { 
         start <- TRUE 
-        curage <- tab[iso[l],1] 
+        curage <- tab[l,1] 
+        next
       } 
-      if (ex[ind[i]]==0) {next;} 
+      if (ex[i]==0) next 
       if (start)  { 
-        if (found == FALSE)  { 
-          bra1 <- bra1 + k*(tab[ind[i],1]-curage) 
-          if (isanc[ind[i]])  { 
-            found <- TRUE 
-          } 
+        if (!found)  { 
+          bra1 <- bra1 + k*(tab[i,1]-curage) 
+          if (isanc[i])  found <- TRUE 
         } 
-        bra2 <- bra2 + k*(tab[ind[i],1]-curage) 
+        bra2 <- bra2 + k*(tab[i,1]-curage) 
       } 
-      curage <- tab[ind[i],1] 
-      if (isiso[ind[i]])  { 
-        k <- k + 1 
-      } else { k <- k-ex[ind[i]] + 1 
-      } 
+      curage <- tab[i,1] 
+      if (isiso[i])  k <- k + 1 else k <- k-ex[i] + 1
     } 
     p <- p-log(rate)-bra1/rate-log(1-exp(-bra2/rate))
-    if (k != 1)  { 
-      print('errorHere')
+    if (k != 1) print('errorHere')
+    if (bra1 >= bra2)  print('errorThere')
+    cur <- l
+    while (TRUE) {
+      if (ex[cur] == 1) {ex[cur] <- 2;break}
+      ex[cur] <- 1
+      cur <- tab[cur,2]
     } 
-    if (bra1 >= bra2)  { 
-      print('errorThere')
-    } 
-    cur <- iso[l];while (tab[cur,2] > 0) {if (ex[cur] == 1) {ex[cur] <- 2;break;};ex[cur] <- 1;cur <- tab[cur,2];} 
   }
+  if (min(ex)==0) print('error: some nodes have not been activated')
+  if (length(which(ex==2))!=(length(iso)-1)) print('error: some internal nodes have not been activated twice')
   return(p)
 }
--- trunk/test/test.R
+++ trunk/test/test.R
@@ -1,25 +1,26 @@
-set.seed(2)
-simu <- simulateOutbreak(R=1,neg=100/365,pi=0.5)
-#simu[,1]=simu[,1]*10
+set.seed(0)
+simu <- simulateOutbreak(R=1,neg=100/365,pi=0.5,ninf=20)
+
 pi=0.5
 start <- Sys.time ()
-record<-inferTTree(ptreeFromFullTree(simu),mcmcIterations=10000,startPi=pi,updatePi=FALSE,testing=TRUE)
-cat('Time spent: ',Sys.time()-start,'\n')
+record<-inferTTree(ptreeFromFullTree(simu),mcmcIterations=10000,startPi=pi,updatePi=FALSE,testing=FALSE)
+print(Sys.time()-start)
+
 #par(mfrow=c(2,2))
 #plot(sapply(record,function(x) x$pTTree+x$pPTree),ylab='Posterior probability',xlab='MCMC iterations',type='l')
 #hist(sapply(record,function(x) x$pi),xlab='Posterior of pi',main='Prior is Unif(0,1)')
 #hist(sapply(record,function(x) x$neg),xlab='Posterior of Ne*g',main='Prior is Exp(1)')
 #hist(sapply(record,function(x) x$R),xlab='Posterior of R',main='Prior is Exp(1)')
 
-ncases=c()
-for (i in 5000:10000) {
-  ptree=ttreeFromFullTree(record[[i]]$tree)
-  ncases=c(ncases,nrow(ptree))
-}
-par(mfrow=c(2,1))
-ncases2=ncases;ncases2[which(ncases2>100)]=100
-hist(ncases2,breaks=0.5:100.5,main='Inferred number of cases')
-plot(dbinom(10,1:100,pi),type='h',main='Theoretical distribution for number of cases')
+# ncases=c()
+# for (i in 5000:10000) {
+#   ptree=ttreeFromFullTree(record[[i]]$tree)
+#   ncases=c(ncases,nrow(ptree))
+# }
+# par(mfrow=c(2,1))
+# ncases2=ncases;ncases2[which(ncases2>100)]=100
+# hist(ncases2,breaks=0.5:100.5,main='Inferred number of cases')
+# plot(dbinom(10,1:100,pi),type='h',main='Theoretical distribution for number of cases')
 #The inferred and theoretical distributions should match!
 
 #TODO: check that in the testing conditions, transmission events are distributed uniformly on the ptree
