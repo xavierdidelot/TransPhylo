c5b8a5ce2f97d141b2dfb7e480629515aac41c26
xavier.didelot@gmail.com <xavier.didelot@gmail.com@3ae39ca0-168b-8fd5-bc72-1a53c72e3606>
2014-09-22 12:35:49 +0000

    Accounting for unobserved branches

--- trunk/R/probTTree.R
+++ trunk/R/probTTree.R
@@ -5,16 +5,22 @@
 #' @param pi probability of sampling an infected individual
 #' @return Probability of the transmission tree
 probTTree = function(ttree,R,pi)  {
-  #TODO need to account for unobserved branches, cf writelatex file
   prob <- 0 
   n <- nrow(ttree)
+  p0 <- -lambert_W0(R*(pi-1)/exp(R))/R
+  pd0 <- rep(NA, n+1)
   for (i in (1:n)) { 
     if (is.na(ttree[i,2])) {prob<-prob+log(1-pi)} else 
     {prob<-prob+log(pi);
     prob <- prob + dgamma((ttree[i,2]-ttree[i,1]), shape = 2, scale = 1, log=TRUE)} 
-    offspring <- which( cbind(ttree[ ,3]) == i ) 
-    prob <- prob + dpois(length(offspring),R,log=TRUE)
-    for (j in (offspring)) {
+    offspring <- which( ttree[ ,3] == i ) 
+    d0 <- length(offspring)
+    if (is.na(pd0[d0+1])) {
+      pd0[d0+1]=d0*log(R)+R*(p0-1)-d0*log(R*p0)+log(1-d0*gamma_inc(d0,R*p0)/gamma(1+d0))
+    }
+    prob <- prob + pd0[d0+1]
+    #prob <- prob + dpois(length(offspring),R,log=TRUE)    
+    for (j in offspring) {
       prob <- prob + dgamma((ttree[j,1]-ttree[i,1]), shape = 2, scale = 1,log=TRUE) 
     } 
   } 
