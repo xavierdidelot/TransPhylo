56e878c7f6ecf7e44dc17cb951334551b2d75ed1
xavierdidelot <xavier.didelot@gmail.com>
2015-07-16 09:43:11 +0100

    Clarify probTTree

--- R/probTTree.R
+++ R/probTTree.R
@@ -9,26 +9,23 @@
 probTTree = function(ttree,off.r,off.p,pi,w.shape,w.scale)  {
   prob <- 0 
   n <- nrow(ttree)
-  f <- function(x) {x-(1-pi)*((1-off.p)/(1-off.p*x))^off.r}
-  p0 <- uniroot(f,c(0,1))$root
-  pd0 <- rep(NA, n+1)
+  omegaStar <- uniroot(function(x) {x-(1-pi)*((1-off.p)/(1-off.p*x))^off.r},c(0,1))$root #This is Equation (1)
+  alphaStar <- rep(NA, n+1)
   for (i in (1:n)) { 
-    if (is.na(ttree[i,2])) prob<-prob+log(1-pi) else 
-      prob<-prob+log(pi)+dgamma((ttree[i,2]-ttree[i,1]),shape=w.shape,scale=w.scale,log=TRUE) 
+    if (is.na(ttree[i,2])) prob<-prob+log(1-pi) #This is the first term in the product in Equation (5)
+    else prob<-prob+log(pi)+dgamma((ttree[i,2]-ttree[i,1]),shape=w.shape,scale=w.scale,log=TRUE) #This is the second term in the product in Equation (5)
     offspring <- which( ttree[ ,3] == i ) 
     d0 <- length(offspring)
-    if (is.na(pd0[d0+1])) {pd0[d0+1]=0
-    notinf=d0+2*off.r*off.p/(1-off.p)
-    for (k in d0:notinf) pd0[d0+1]=pd0[d0+1]+choose(k,d0)*dnbinom(k,off.r,off.p)*p0^{k-d0}
-    pd0[d0+1]=log(pd0[d0+1])
+    if (is.na(alphaStar[d0+1])) {
+      alphaStar[d0+1]=0
+      notinf=d0+2*off.r*off.p/(1-off.p)
+      for (k in d0:notinf) alphaStar[d0+1]=alphaStar[d0+1]+choose(k,d0)*dnbinom(k,off.r,off.p)*omegaStar^{k-d0} #This is in Equation (2)
+      alphaStar[d0+1]=log(alphaStar[d0+1])
     }
-      #R*(p0-1)-d0*log(p0)+pgamma(R*p0,d0,log.p=T)#This is log of alpha_*(d)
-    prob <- prob + pd0[d0+1]
-    #prob <- prob + dpois(length(offspring),R,log=TRUE)    
+    prob <- prob + alphaStar[d0+1] #This is the third term in the product in Equation (5)
     for (j in offspring) {
-      prob <- prob + dgamma((ttree[j,1]-ttree[i,1]),shape=w.shape,scale=w.scale,log=TRUE) 
+      prob <- prob + dgamma((ttree[j,1]-ttree[i,1]),shape=w.shape,scale=w.scale,log=TRUE) #This is the fourth term in the product in Equation (5)
     } 
   } 
-#  print(c(p0,exp(pd0[1])*(1-pi)))#Sanity check: should be the same
   return(prob)
 } 
