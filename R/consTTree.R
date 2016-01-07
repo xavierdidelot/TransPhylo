3ab9cb435e3ad4e333f897aa04897869d5704ee4
xavierdidelot <xavier.didelot@gmail.com>
2016-01-07 14:35:52 +0000

    Working on consensus function

--- R/consTTree.R
+++ R/consTTree.R
@@ -7,10 +7,10 @@ consTTree = function(record,burnin=0.5)
   record=record[round(length(record)*burnin):length(record)]
   m=length(record)
   n=sum(record[[1]]$tree[,2]==0&record[[1]]$tree[,3]==0) 
-  
+
   #Record partitions in sampled transmission tree
-  hash=vector('list',n*m)
-  a=floor(n*runif(n))+1
+  hash=vector('list',n*m*10)
+  a=floor(n*10*runif(n))+1
   for (i in 1:m)
   {
     ttree=ttreeFromFullTree(record[[i]]$tree)
@@ -24,20 +24,41 @@ consTTree = function(record,burnin=0.5)
       children[w,]=colSums(children[direct,,drop=F])>0
       if (is.na(children[w,1])) next
       if (w<=n) children[w,w]=1
-    if (ttree[w,3]>0) todo=c(todo,ttree[w,3])
+      if (ttree[w,3]>0) todo=c(todo,ttree[w,3])
     }
     #Add nodes one by one
     for (j in 1:nrow(ttree)) {
-      c=children[[j]]
-      hh=1+mod(sum(a[c]),m*n)
+      c=children[j,]
+      hh=1+(sum(a[c])%%(length(hash)))
       found=0
-      for (k in 1:length(hash[[hh]]))
-        if (children[j,]==hash[hh]$c) {found=k;break}
-      
+      while (1) {
+        if (is.null(hash[[hh]])) break
+        if (all(children[j,]==hash[[hh]]$c)) break
+        hh=hh+1
+        if (hh>length(hash)) hh=1
+      }
+      if (is.null(hash[[hh]])) {hash[[hh]]$c=children[j,];hash[[hh]]$n=0;hash[[hh]]$w=c();hash[[hh]]$last=0}
+      if (hash[[hh]]$last==i) hash[[hh]]$w[1]=hash[[hh]]$w[1]+1
+      else {
+        hash[[hh]]$last=i
+        hash[[hh]]$n=hash[[hh]]$n+1
+        hash[[hh]]$w=c(1,hash[[hh]]$w)
+      }
     }
   }
   
   #Choose partitions to include in consensus transmission tree
-  cons=ttree
-  return(cons)
+  comb=hash[!sapply(hash,is.null)]
+  comb=comb[order(sapply(comb,'[[',2),decreasing=T)]
+  keep=c(1)
+  for (i in 2:length(comb)) {
+    c1=which(comb[[i]]$c==1)
+    exclude=F
+    for (j in keep) {
+      c2=which(comb[[j]]$c==1)
+      if (length(intersect(c1,c2)>0) && length(setdiff(c1,c2))>0 && length(setdiff(c2,c1))>0) {exclude=T;print('excluded');break}
+    }
+    if (exclude==F) keep=c(keep,i)
+  }
+ return(comb)
 }
